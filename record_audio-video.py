20ad6ac4c84ff3dea995dd2d1fc06f50c07c334f
Mavis <mavis.murdock@tufts.edu>
2025-05-07 11:57:38 -0400

    clean up file

--- record_audio-video.py
+++ record_audio-video.py
@@ -8,16 +8,9 @@ import asyncio
 CAMERA_IDS = [0, 1]
 CAMERA_OUTPUT_FILES = ['camera0_output.mp4', 'camera1_output.mp4']
 
-AUDIO_OUTPUT_FILE = "output_audio.wav"
+AUDIO_OUTPUT_FILE = "output_audio"
 NUM_CHANNELS = 2 # use >1 channel when using m-audio
 #--------------------------------------------------------------------
-
-# async def wait_for_q_to_quit(audio_recorder, video_recorder):
-#     print("[INFO] Press 'q' then Enter to stop recording...")
-#     loop = asyncio.get_running_loop()
-#     await loop.run_in_executor(None, input, ">> ")
-#     audio_recorder.stop()
-#     video_recorder.stop()
 
 async def wait_for_q_to_quit(audio_recorder, video_recorder):
     print("[INFO] Press 'q' then Enter to stop recording...")
